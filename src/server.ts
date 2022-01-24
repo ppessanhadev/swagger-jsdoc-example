0f97da8433e4de649ebd086f91c2aa1767e488d7
Pablo Pessanha <pablopessanh@gmail.com>
2022-01-24 13:49:08 -0300

    feat: adds swagger configs and swagger jsdocs config

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,26 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import swaggerJSDocs from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
 
 const app = express();
+app.use(express.json());
 
-app.use(cors());
-app.use(express.json())
+// Swagger configs
+const swaggerDocs = swaggerJSDocs({
+  swaggerDefinition: {
+    info: {
+      title: "Example Swagger automator API",
+      version: "1.0.0",
+    },
+  },
+  apis: ["server.ts"],
+});
 
-app.get('/', (_req, res) => {
-  return res.status(200).json({ message: 'ok' });
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+// Routes
+app.get("/", (_req, res) => {
+  return res.status(200).send({ message: "ok" });
 });
 
 app.listen(3333);
